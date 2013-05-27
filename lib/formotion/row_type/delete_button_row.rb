75c62f07df5f31ab6ff18ef5acb527edc792a91b
Arkan <florian.bertholin@gmail.com>
2013-05-27 15:28:08 +0200

    Add delete button

---
+++ lib/formotion/row_type/delete_button_row.rb
@@ -0,0 +1,29 @@
+motion_require 'button'
+
+module Formotion
+  module RowType
+    class DeleteButtonRow < ButtonRow
+
+      def build_cell(cell)
+        super
+        delete_button = UIButton.buttonWithType(UIButtonTypeCustom)
+        delete_button.frame = cell.frame.tap do |f|
+          f.origin.x = 10     # 10 pixels left
+          f.size.width -= 20  # 10 pixels right
+          f.size.height += 1  # To remove the bottom line of the Cell
+        end
+        delete_button.setTitle(cell.textLabel.text, forState: UIControlStateNormal)
+        delete_button.setBackgroundImage(UIImage.imageNamed("formotion/button_red.png"), forState:UIControlStateNormal)
+        delete_button.titleLabel.font = UIFont.boldSystemFontOfSize(16)
+        delete_button.titleLabel.color = UIColor.whiteColor
+        delete_button.autoresizingMask = UIViewAutoresizingNone | UIViewAutoresizingFlexibleWidth
+        delete_button.addTarget(self, action: :on_delete_pressed, forControlEvents: UIControlEventTouchUpInside)
+        cell.addSubview(delete_button)
+      end
+
+      def on_delete_pressed
+        self.row.on_tap_callback.call(self.row) if self.row.on_tap_callback
+      end
+    end
+  end
+end
