3ff6e56887fb73fa5e2549ca16c76f09d7570342
Arkan <florian.bertholin@gmail.com>
2013-09-20 17:07:55 +0200

    DeleteButtonRow is now only visually comptatible with iOS7

--- lib/formotion/row_type/delete_button_row.rb
+++ lib/formotion/row_type/delete_button_row.rb
@@ -6,23 +6,7 @@ module Formotion
 
       def build_cell(cell)
         super
-        delete_button = UIButton.buttonWithType(UIButtonTypeCustom)
-        delete_button.frame = cell.frame.tap do |f|
-          f.origin.x = Formotion::RowType::Base.field_buffer / 2
-          f.size.width -= Formotion::RowType::Base.field_buffer
-          if UIDevice.currentDevice.systemVersion < "7.0"
-            f.size.height += 2 # To remove the bottom line of the Cell
-          end
-        end
-        delete_button.setTitle(cell.textLabel.text, forState: UIControlStateNormal)
-        delete_button.setBackgroundImage(UIImage.imageNamed("formotion/button_red.png"), forState:UIControlStateNormal)
-        delete_button.titleLabel.font = UIFont.boldSystemFontOfSize(16)
-        delete_button.titleLabel.color = UIColor.whiteColor
-        delete_button.autoresizingMask = UIViewAutoresizingNone | UIViewAutoresizingFlexibleWidth
-        delete_button.addTarget(self, action: :on_delete_pressed, forControlEvents: UIControlEventTouchUpInside)
-        delete_button.backgroundColor = UIColor.clearColor
-        cell.backgroundColor = UIColor.clearColor
-        cell.addSubview(delete_button)
+        cell.textLabel.textColor = 0xFF332A.uicolor
       end
 
       def on_delete_pressed
